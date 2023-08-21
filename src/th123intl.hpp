6b790da62e267ab3c22508fdcf2095b74cafced2
Enebe <enebe.nb@gmail.com>
2023-08-21 13:05:37 -0300

    convert header

--- src/th123intl.hpp
+++ src/th123intl.hpp
@@ -31,32 +31,36 @@ namespace th123intl {
     inline void ConvertCodePage(unsigned int fromCP, const std::string_view& from, unsigned int toCP, std::string& to) {
         if (fromCP == toCP) { to = from; return; }
         else {
+            DWORD dwFlags = fromCP == CP_UTF8 ? inFlags & WC_ERR_INVALID_CHARS : inFlags;
             std::wstring buffer;
-            size_t size = MultiByteToWideChar(fromCP, inFlags, from.data(), from.size(), NULL, NULL);
+            size_t size = MultiByteToWideChar(fromCP, dwFlags, from.data(), from.size(), NULL, NULL);
             buffer.resize(size);
-            if (!MultiByteToWideChar(fromCP, inFlags, from.data(), from.size(), buffer.data(), buffer.size()))
+            if (!MultiByteToWideChar(fromCP, dwFlags, from.data(), from.size(), buffer.data(), buffer.size()))
                 { to.resize(0); return; }
 
-            size = WideCharToMultiByte(toCP, outFlags, buffer.data(), buffer.size(), NULL, NULL, NULL, NULL);
+            dwFlags = toCP == CP_UTF8 ? outFlags & WC_ERR_INVALID_CHARS : outFlags;
+            size = WideCharToMultiByte(toCP, dwFlags, buffer.data(), buffer.size(), NULL, NULL, NULL, NULL);
             to.resize(size);
-            if (!WideCharToMultiByte(toCP, outFlags, buffer.data(), buffer.size(), to.data(), to.size(), NULL, NULL))
+            if (!WideCharToMultiByte(toCP, dwFlags, buffer.data(), buffer.size(), to.data(), to.size(), NULL, NULL))
                 { to.resize(0); return; }
         }
     }
 
     template<int flags = WC_COMPOSITECHECK|WC_DEFAULTCHAR>
     inline void ConvertCodePage(const std::wstring_view& from, unsigned int toCP, std::string& to) {
-        size_t size = WideCharToMultiByte(toCP, flags, from.data(), from.size(), NULL, NULL, NULL, NULL);
+        const DWORD dwFlags = toCP == CP_UTF8 ? flags & WC_ERR_INVALID_CHARS : flags;
+        size_t size = WideCharToMultiByte(toCP, dwFlags, from.data(), from.size(), NULL, NULL, NULL, NULL);
         to.resize(size);
-        if (!WideCharToMultiByte(toCP, flags, from.data(), from.size(), to.data(), to.size(), NULL, NULL))
+        if (!WideCharToMultiByte(toCP, dwFlags, from.data(), from.size(), to.data(), to.size(), NULL, NULL))
             { to.resize(0); return; }
     }
 
     template<int flags = MB_USEGLYPHCHARS>
     inline void ConvertCodePage(unsigned int fromCP, const std::string_view& from, std::wstring& to) {
-        size_t size = MultiByteToWideChar(fromCP, flags, from.data(), from.size(), NULL, NULL);
+        const DWORD dwFlags = fromCP == CP_UTF8 ? flags & WC_ERR_INVALID_CHARS : flags;
+        size_t size = MultiByteToWideChar(fromCP, dwFlags, from.data(), from.size(), NULL, NULL);
         to.resize(size);
-        if (!MultiByteToWideChar(fromCP, flags, from.data(), from.size(), to.data(), to.size()))
+        if (!MultiByteToWideChar(fromCP, dwFlags, from.data(), from.size(), to.data(), to.size()))
             { to.resize(0); return; }
     }
 }
